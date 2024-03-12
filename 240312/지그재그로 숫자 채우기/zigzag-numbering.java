b3acfc3431145d1019c9c25b364ba16b956be2b8
Janghan Bae <102276240+newbieJanghan@users.noreply.github.com>
2024-03-12 17:30:38 +0900

    Title: 지그재그로 숫자 채우기, Time: 212ms, Memory: 12MB, Status: Passed - CodeTree

--- 240312/지그재그로 숫자 채우기/zigzag-numbering.java
+++ 240312/지그재그로 숫자 채우기/zigzag-numbering.java
@@ -1,22 +1,39 @@
 import java.util.Scanner;
 
 public class Main {
-
-    public static void main(String[] receipt) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // n, m을 입력받습니다.
         int n = sc.nextInt();
         int m = sc.nextInt();
 
-        int[][] arr2d = new int[n][m];
+        // 2차원 배열을 구현합니다.
+        int[][] arr = new int[100][100];
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                boolean isReverse = j % 2 == 1;
-                if (isReverse) {
-                    System.out.print((n * (j + 1) - (i + 1)) + " ");
-                } else {
-                    System.out.print((i + j * n) + " ");
+        // Step 1:
+        int count = 0;
+        for(int col = 0; col < m; col++) {
+            if (col % 2 == 0){
+                // Case 1:
+                for(int row = 0; row < n; row++){
+                    arr[row][col] = count;
+                    count++;
                 }
+            }
+            else {
+                // Case 2:
+                for(int row = n - 1; row >= 0; row--){
+                    arr[row][col] = count;
+                    count++;
+                }
+            }
+        }
+                
+        // 출력:
+        for(int row = 0; row < n; row++) {
+            for(int col = 0; col < m; col++) {
+                System.out.print(arr[row][col] + " ");
             }
             System.out.println();
         }
