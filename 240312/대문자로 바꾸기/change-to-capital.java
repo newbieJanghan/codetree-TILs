016dde105a3e5c4faa90819a96215d9949203ebf
Janghan Bae <102276240+newbieJanghan@users.noreply.github.com>
2024-03-12 16:58:37 +0900

    Title: 대문자로 바꾸기, Time: 177ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240312/대문자로 바꾸기/change-to-capital.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] receipt) {
+        Scanner sc = new Scanner(System.in);
+        String[][] arr2d = new String[5][3];
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 3; j++) {
+                arr2d[i][j] = sc.next().toUpperCase();
+            }
+        }
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(arr2d[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
