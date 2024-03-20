fb26de438df2ff3127538f6141bb28a87748f077
Janghan Bae <102276240+newbieJanghan@users.noreply.github.com>
2024-03-20 14:29:04 +0900

    Title: 그 전 알파벳, Time: 104ms, Memory: 10MB, Status: Passed - CodeTree

--- 240320/그 전 알파벳/before-the-alphabet.java
+++ 240320/그 전 알파벳/before-the-alphabet.java
@@ -6,7 +6,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int input = (int) sc.next().charAt(0);
 
-        if (input == 0) {
+        if (input == 97) {
             System.out.print("z");
         } 
         else {
