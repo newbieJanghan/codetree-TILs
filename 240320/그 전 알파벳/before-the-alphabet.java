0a243a80fa572a034a24b74d0c443c4ebda37b11
Janghan Bae <102276240+newbieJanghan@users.noreply.github.com>
2024-03-20 14:27:19 +0900

    Title: 그 전 알파벳, Time: 136ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240320/그 전 알파벳/before-the-alphabet.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int input = (int) sc.next().charAt(0);
+
+        if (input == 0) {
+            System.out.print("z");
+        } 
+        else {
+            System.out.print((char) (input-1));
+        }
+    }
+}
